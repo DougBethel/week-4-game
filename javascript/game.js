8fe0643aab1b8038463b96ff055eb48865e6e3be
doug.bethel@icloud.com <31324846+Rawkin27@users.noreply.github.com>
2017-12-14 15:44:21 -0500

    final draft

---
+++ javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+var computerNumber = Math.floor(Math.random() * 100)
+var random1
+var random2
+var random3
+var random4
+
+function generateRandomNumber () {
+  // create number and div
+  console.log(computerNumber);
+}
+
+
+
+
+
+
+
+
+
+
+
+});
